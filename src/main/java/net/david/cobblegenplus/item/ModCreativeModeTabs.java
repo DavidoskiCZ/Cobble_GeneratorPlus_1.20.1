aaca4e7b9454919b79f6b0f8bdfefc31a721252e
David Romer <romerda@student.uzlabina.cz>
2023-12-12 23:04:30 +0100

    Added Test Item and Test Creative Tab + translation + textures

---
+++ src/main/java/net/david/cobblegenplus/item/ModCreativeModeTabs.java
@@ -0,0 +1,32 @@
+package net.david.cobblegenplus.item;
+
+import net.david.cobblegenplus.CobbleGenPlus;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, CobbleGenPlus.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> TEST_TAB = CREATIVE_MODE_TABS.register("test_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.TESTITEM.get()))
+                    .title(Component.translatable("creativetab.test_tab"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.TESTITEM.get());
+
+                        //test přidání random itemu z vanily do tabu
+                        output.accept(Items.DIAMOND);
+                    })
+                    .build());
+
+    public static void register(IEventBus eventBus)
+    {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
